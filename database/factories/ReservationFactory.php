5cc061469a7798f3808bc0a068fb18de2f0f563c
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-05-18 19:55:00 +0200

    Prepare Factories

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Office;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,12 @@ class ReservationFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'office_id' => Office::factory(),
+            'price' => $this->faker->numberBetween(10_000, 20_000),
+            'status' => 1,
+            'start_date' => now()->addDay(1)->format('Y-m-d'),
+            'end_date' => now()->addDay(5)->format('Y-m-d'),
         ];
     }
 }
